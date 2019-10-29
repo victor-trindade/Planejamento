9b11d833a7e5cfde4e116a1aace4fdbd3a302e96
Victor Trindade <victor.trindade@outlook.com.br>
2019-10-29 00:02:50 -0300

    Ajustes no template

--- home/url.py
+++ home/url.py
@@ -3,7 +3,7 @@ from .views import MonthArchiveViewPage, WeekArchiveViewPage
 
 
 urlpatterns = [
-    path('<int:year>/year/<int:month>/', MonthArchiveViewPage.as_view(month_format='%m'), name="archive_month"),
-    path('<int:year>/<int:week>/', WeekArchiveViewPage.as_view(), name="archive_week"),
+    path('<int:year>/<int:month>/', MonthArchiveViewPage.as_view(month_format='%m'), name="archive_month"),
+    path('week/<int:year>/<int:week>/', WeekArchiveViewPage.as_view(), name="archive_week"),
 
 ]
--- home/views.py
+++ home/views.py
@@ -6,10 +6,12 @@ from django.views.generic.dates import MonthArchiveView,WeekArchiveView
 
 class MonthArchiveViewPage(MonthArchiveView):
 
-    queryset = Store.objects.all()
+
     template_name = 'home/month_archive.html'
     date_field = "visit_day"
     allow_future = True
+    def get_queryset(self,  **kwargs):
+        return Store.objects.all().order_by('coordinator__name','-visit_day')
 
 class WeekArchiveViewPage(WeekArchiveView):
 
